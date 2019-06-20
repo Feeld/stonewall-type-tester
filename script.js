612e137130ed21b827256c77095d23c6785414de
Dario Villanueva Ablanedo <dario@alolo.co>
2019-06-20 15:07:59 +0100

    adds colours and better export

--- script.js
+++ script.js
@@ -3,15 +3,27 @@
 (function() {
   // prints "hi" in the browser's dev tools console
   const slider = document.querySelector('#font-size-slider');
+  const colorPicker = document.querySelector('.colorPicker');
   const textInput = document.querySelector('#font-input');
   const downloadLink = document.querySelector('#download-link')
   const canvasContainer = document.querySelector('#canvasContainer');
   const viewerContainer = document.querySelector('.viewer');
   const viewerContainer2 = document.querySelector('.viewer2');
+  const hiddenCanvas = document.createElement('canvas');
+  const canvas = document.createElement('canvas');
+
+  viewerContainer.appendChild(hiddenCanvas);
+  viewerContainer2.appendChild(canvas);
 
   slider.addEventListener('input', () => {
     textInput.style['font-size'] = slider.value + 'px'
   })
+
+  const colors = {
+    black: "#111913",
+    white: "#FFFFFF",
+    red: "#FF401D",
+  }
 
   function getLines(ctx, text, maxWidth) {
     var words = text.split(" ");
@@ -32,8 +44,9 @@
     return lines;
   }
 
-  const canvas = document.createElement('canvas');
-  viewerContainer2.appendChild(canvas);
+  let bgColor = colors.white;
+  let fgColor = colors.black;
+
 
   function draw() {
     const textInputStyle = window.getComputedStyle(textInput)
@@ -42,16 +55,22 @@
     const contextLink = this;
     const ctx = canvas.getContext('2d');
     const desiredFontSize = slider.value * 0.85;
-    ctx.fillStyle = "#fff"
+    ctx.fillStyle = bgColor;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "#000"
+    ctx.fillStyle = fgColor;
     ctx.font = `${desiredFontSize}px Stonewall`
     ctx.textBaseline = 'bottom'
     ctx.textAlign = 'center'
 
-    const lines = getLines(ctx, textInput.innerText, canvas.width);
+    const ps = [];
 
+    textInput.querySelectorAll('p').forEach(p => ps.push(p.innerText))
+
+    const lines = ps.reduce((ls, p) => {
+      return ls.concat(getLines(ctx, p, canvas.width))
+    }, []);
+    debugger;
     let step = 16;
     let fontSize = desiredFontSize;
     for (const line of lines) {
@@ -71,32 +90,79 @@
     return step + 16;
   }
 
-  const hiddenCanvas = document.createElement('canvas');
-
-  viewerContainer.appendChild(hiddenCanvas);
 
   function download() {
 
     const canvasHeight = draw();
-    hiddenCanvas.height = Math.min(canvasHeight, canvas.height);
+    hiddenCanvas.height = Math.max(canvasHeight, canvas.height);
     hiddenCanvas.width = canvas.width;
 
-    console.log(canvasHeight, canvas.width)
-
+    console.log('exporting: ', canvasHeight, canvas.width)
 
     var ctx = hiddenCanvas.getContext('2d');
-    ctx.fill = '#fff';
+    ctx.fillStyle = bgColor;
+    ctx.clearRect(0, 0, hiddenCanvas.width, hiddenCanvas.height);
     ctx.fillRect(0, 0, hiddenCanvas.width, hiddenCanvas.height);
+
+    const startY = (hiddenCanvas.height - canvasHeight) / 2
+
     ctx.drawImage(
       canvas,
       0,
-      0,
+      startY,
     );
 
     var dataURL = hiddenCanvas.toDataURL();
     this.href = dataURL;
 
   };
-  downloadLink.addEventListener('click', download);
+
+
+  function handleChangeColor(e) {
+    e.preventDefault();
+    if (!e.target.classList.contains('color')) {
+      return;
+    }
+
+    if (e.target.classList.contains('selected')) {
+      return;
+    }
+
+    const type = e.target.classList.contains('fg') ? 'fg' : 'bg';
+
+    colorPicker.querySelector(`.${type}.color.selected`)
+      .classList.remove('selected');
+    e.target.classList.add('selected');
+
+    const colorList = Object.keys(colors);
+
+    const selectedColor = colorList.find(c => e.target.classList.contains(c));
+
+    if (type === 'bg') {
+      bgColor = selectedColor;
+    } else {
+      fgColor = selectedColor;
+    }
+
+    setColors();
+
+  }
+
+
+  function setColors() {
+    textInput.style['color'] = fgColor;
+    textInput.style['background-color'] = bgColor;
+  }
+
+
+  function startup() {
+    console.log('starting up')
+    downloadLink.addEventListener('click', download);
+    colorPicker.addEventListener('click', handleChangeColor);
+    setColors();
+  }
+
+  window.onload = startup;
+
 })()
 

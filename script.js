7f0eeeba05252df4fc194b75b62d1c9858d417e2
Dario Villanueva Ablanedo <dario@alolo.co>
2019-06-21 15:50:40 +0100

    ensure we replace with octet stream

--- script.js
+++ script.js
@@ -147,7 +147,7 @@
 
 
     let a = document.createElement("a");
-    a.href = hiddenCanvas.toDataURL("image/png");
+    a.href = hiddenCanvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
     a.setAttribute("download", "FreeToBe.png");
     document.body.appendChild(a);
     a.click();
